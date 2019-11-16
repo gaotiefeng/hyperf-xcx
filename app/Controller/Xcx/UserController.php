82b3be3b1dd7c9c54734e421da5e57a189919ec9
gaotiefeng <1096392101@qq.com>
2019-11-16 14:17:26 +0800

    add xcx.config

--- app/Controller/Xcx/UserController.php
+++ app/Controller/Xcx/UserController.php
@@ -21,19 +21,8 @@ class UserController extends IndexController
      */
     public function login()
     {
-        $config = [
-            'app_id' => 'wx45d028d42222a99d',
-            'secret' => '41e617fbfab0a1b244abf0a7a92e8c4a',
 
-            // 下面为可选项
-            // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
-            'response_type' => 'array',
-
-            'log' => [
-                'level' => 'debug',
-                'file' => __DIR__.'/wechat.log',
-            ],
-        ];
+        $config = config('xcx');
         $code = $this->request->input('code');
         $app = Factory::miniProgram($config);
 
---
+++ config/autoload/xcx.php
@@ -0,0 +1,15 @@
+<?php
+
+return  [
+    'app_id' => 'wx45d028d42222a99d',
+    'secret' => '41e617fbfab0a1b244abf0a7a92e8c4a',
+
+    // 下面为可选项
+    // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
+    'response_type' => 'array',
+
+    'log' => [
+        'level' => 'debug',
+        'file' => BASE_PATH.'/runtime/xcx/wechat.log',
+    ],
+];
