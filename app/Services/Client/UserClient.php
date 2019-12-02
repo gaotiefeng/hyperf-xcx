dcee413724d1d8810ae29c4a90c5cd4dc1aa2de5
gaotiefeng <1096392101@qq.com>
2019-12-02 15:56:51 +0800

    add tempalte

--- app/Services/Client/UserClient.php
+++ app/Services/Client/UserClient.php
@@ -58,7 +58,6 @@ class UserClient
     {
         $client = $this->reload();
             $params = [
-                'access_token' => $accessToken,
                 'touser' => $openId,
                 'template_id' => $this->remarkId,
                 'form_id' => $formId,
@@ -67,6 +66,7 @@ class UserClient
                     'keyword1' => ['value'=>'keyword1'],
                     'keyword2' => ['value'=>'keyword2']
                 ],
+                'emphasis_keyword' => 'keyword1',
             ];
         // TODO get ['query' => []]  post ['form_params' => $params]
         return $client->post('/cgi-bin/message/wxopen/template/send?access_token='.$accessToken, ['query' => $params])->getBody()->getContents();
