44efa9f8cbad383369019f2d2cbea40fe06975c3
gaotiefeng <1096392101@qq.com>
2020-04-02 15:46:05 +0800

    update log

--- .build/log.delete.sh
+++ .build/log.delete.sh
@@ -5,10 +5,11 @@ LOG_DIR=$( cd "$( dirname "${BASH_SOURCE[1]}" )" && pwd )
 HYPERF_LOG="$LOG_DIR/runtime/logs/hyperf.log"
 
 deleteLog() {
-      echo > "${HYPERF_LOG}"
-      if [ -s ${HYPERF_LOG} ]; then
-          echo "${HYPERF_LOG}->文件已经清空"
+      if [ -e ${HYPERF_LOG} ]; then
+          echo > "${HYPERF_LOG}"
+          echo "log delete"
       fi
+
 }
 
 deleteLog
